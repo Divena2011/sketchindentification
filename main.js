3c18c8059341fe0dd94fbdcbcbce26a6f7a0a9da
Divena2011 <91365165+Divena2011@users.noreply.github.com>
2021-12-18 16:58:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup(){
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis;
+}
+function preload(){
+    classifier= ml5.imageClassifier('DoodleNet');
+}
+function clearCanvas(){
+    background("white");
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+ function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML='Label: '+ results[0].label;
+    document.getElementById('confidence').innerHTML='confidence: '+ Math.round(results[0].confidence*100)+'%';
+
+    UtterThis= new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
